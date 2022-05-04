f5056848251c1701c9bbb3c82baab4988fc23aa1
cehali <cezary.haliniarz@gmail.com>
2022-05-04 13:07:04 +0200

    feat: handle error and success when creating new

--- frontend/src/pages/NewJsonUrlAsset.tsx
+++ frontend/src/pages/NewJsonUrlAsset.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
 import Editor from "@monaco-editor/react";
 import js from 'jsonpath';
 import Card from "../components/Card";
 import { NewJsonUrlAssetInput } from "../types";
+import Modal from "../components/Modal";
 
 const NewJsonUrlAsset = () => {
+	const navigate = useNavigate();
 	const [url, setUrl] = useState('');
 	const [jsonpath, setJsonpath] = useState('');
 	const [stringJson, setStringJson] = useState('');
 	const [jsonpathMatchResult, setJsonpathMatchResult] = useState('');
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	useEffect(() => {
 		if (stringJson) {
@@ -55,72 +59,83 @@ const NewJsonUrlAsset = () => {
 		const backendUrl = process.env.BACKEND_URL;
 		const url = `${backendUrl}/custom-urls`;
 		return axios.post(url, newCustomUrl);
+	}, {
+		onError: () => setIsModalOpen(true),
+		onSuccess: () => navigate('/')
 	});
 
 	return (
-		<Card>
-			<div className="mb-2">
-				<label className="block text-sky-900 text-sm font-bold mb-2" htmlFor="url">
-					URL
-				</label>
-				<input
-					className="shadow border rounded w-full py-2 px-3 text-neutral-600 focus:outline-none focus:shadow-outline"
-					id="url" type="text"
-					placeholder="URL"
-					onChange={(event) => setUrl(event.target.value)}
-				/>
-			</div>
-			<div className="mb-2">
-				<label className="block text-sky-900 text-sm font-bold mb-2" htmlFor="jsonpath">
-					JSON path
-				</label>
-				<input
-					className="shadow border rounded w-full py-2 px-3 text-neutral-600  focus:outline-none focus:shadow-outline"
-					id="jsonpath" type="text"
-					placeholder="JSON path"
-					onChange={(event) => setJsonpath(event.target.value)}
-				/>
-			</div>
-			<button
-				onClick={() => handleEvaluate()}
-				className="bg-redstone hover:opacity-75 text-white font-bold py-2 px-4 rounded-full"
-			>
-				Evaluate
-			</button>
-			{stringJson && (
-				<div className="flex justify-between">
-					<Editor 
-						height="40vh"
-						defaultLanguage="json"
-						width="48%"
-						value={JSON.stringify(JSON.parse(stringJson), null, 2)}
-						options={ { minimap: { enabled: false }}}
-						className="border-2"
-					/>
-					<Editor 
-						height="40vh"
-						width="48%"
-						defaultLanguage="json"
-						value={!!jsonpathMatchResult 
-							? JSON.stringify(JSON.parse(jsonpathMatchResult), null, 2) 
-							: "No matches"
-						}
-						options={ { minimap: { enabled: false }}}
-						className="border-2"
+		<div>
+			<Card>
+				<div className="mb-2">
+					<label className="block text-sky-900 text-sm font-bold mb-2" htmlFor="url">
+						URL
+					</label>
+					<input
+						className="shadow border rounded w-full py-2 px-3 text-neutral-600 focus:outline-none focus:shadow-outline"
+						id="url" type="text"
+						placeholder="URL"
+						onChange={(event) => setUrl(event.target.value)}
 					/>
 				</div>
-			)}
-			{jsonpathMatchResult && (
-				<div className="flex justify-end">
-					<button
-						onClick={() => mutation.mutate({ url, jsonpath })}
-						className="bg-redstone hover:opacity-75 text-white font-bold py-2 px-4 rounded-full"
-					>
-						Subscribe
-					</button>
+				<div className="mb-2">
+					<label className="block text-sky-900 text-sm font-bold mb-2" htmlFor="jsonpath">
+						JSON path
+					</label>
+					<input
+						className="shadow border rounded w-full py-2 px-3 text-neutral-600  focus:outline-none focus:shadow-outline"
+						id="jsonpath" type="text"
+						placeholder="JSON path"
+						onChange={(event) => setJsonpath(event.target.value)}
+					/>
 				</div>
-			)}
-		</Card>
+				<button
+					onClick={() => handleEvaluate()}
+					className="bg-redstone hover:opacity-75 text-white font-bold py-2 px-4 rounded-full"
+				>
+					Evaluate
+				</button>
+				{stringJson && (
+					<div className="flex justify-between">
+						<Editor 
+							height="40vh"
+							defaultLanguage="json"
+							width="48%"
+							value={JSON.stringify(JSON.parse(stringJson), null, 2)}
+							options={ { minimap: { enabled: false }}}
+							className="border-2"
+						/>
+						<Editor 
+							height="40vh"
+							width="48%"
+							defaultLanguage="json"
+							value={!!jsonpathMatchResult 
+								? JSON.stringify(JSON.parse(jsonpathMatchResult), null, 2) 
+								: "No matches"
+							}
+							options={ { minimap: { enabled: false }}}
+							className="border-2"
+						/>
+					</div>
+				)}
+				{jsonpathMatchResult && (
+					<div className="flex justify-end">
+						<button
+							onClick={() => mutation.mutate({ url, jsonpath })}
+							className="bg-redstone hover:opacity-75 text-white font-bold py-2 px-4 rounded-full"
+						>
+							Subscribe
+						</button>
+					</div>
+				)}
+			</Card>
+			<Modal
+				isOpen={isModalOpen}
+				setIsOpen={setIsModalOpen}
+				title="There is a problem"
+				text="Introduced pair URL with JSONPath already exists"
+			/>
+		</div>
 	);
 };
 
